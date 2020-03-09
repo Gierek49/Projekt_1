84a47def817ec20eb10a82d5dafa7d2a07ade9ba
Gierek49 <patryk29011@o2.pl>
2020-03-09 18:20:59 +0100

    pobieranie xml

---
+++ Projekt_1/Projekt_1/Downloading/DownloadFromXml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Projekt_1.Downloading
+{
+    class DownloadFromXml
+    {
+        public void downloadXml(string linkUrl)
+        {
+            XElement xml = XElement.Load(linkUrl);
+        }
+    }
+}
