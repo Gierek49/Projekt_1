43f4d185aa2ed6719e5d16c85d31fe146bbea4a5
Fluniks <49128004+Fluniks@users.noreply.github.com>
2020-03-19 13:22:03 +0100

    podstawowy CRUD

---
+++ Projekt_1/Projekt_1/CRUD.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Atributes;
+using MongoDB.Driver;
+
+
+namespace Projekt_1
+{
+    class CRUD
+    {
+        static void Main(string[] args)
+        {
+            MongoCRUD db = new MongoCRUD("AddresBook");
+            Console.ReadLine();
+        }
+    }
+
+    public class MongoCRUD
+    {
+        private IMongoDatabase db;
+
+        public MongoCRUD(string database)
+        {
+            var client = new MongoClient();
+            db = client.GetDatabase(database);
+        }
+
+        public void InsertRecord<T>(string table, T recprd)
+        {
+            var collection = db.GetCollection<T>(table);
+            collection.InsertOne(record);
+        }
+
+        public List<T> LoadRecords<T>(string table)
+        {
+            var collection = db.GetCollection<T>(table);
+
+            return collection.Find(new BsonDocument()).ToList();
+        }
+    }
+}
